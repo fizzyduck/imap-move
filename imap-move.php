3f7e3c623c14be687dd9f458fc9e98e7def3a488
fizzyduck <46020783+fizzyduck@users.noreply.github.com>
2020-12-30 16:11:18 +0000

    Reformat license

--- imap-move.php
+++ imap-move.php
@@ -1,11 +1,14 @@
 #!/usr/bin/php
 <?php
 /**
- * imap-move is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- *
- * imap-move is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with imap-move. If not, see http://www.gnu.org/licenses/.
+ * imap-move is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. * imap-move is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. * You should have received a copy of the GNU General Public License
+ * along with imap-move. If not, see http://www.gnu.org/licenses/.
  *
  * Purpose: Moves Mail from one IMAP account to another
  *
